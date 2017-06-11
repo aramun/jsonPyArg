3c160e0a0a20ce751ec3a40707b081759d32da26
Paolo Di Ciaula <pdiciaula@pic-informatica.it>
2017-06-11 13:49:04 +0200

    Fix bugs

--- cli.py
+++ cli.py
@@ -4,7 +4,7 @@ import sys
 
 class Cli:
     def __init__(self, config_json):
-        self.config = json.loads(utils.readLines(config_json))
+        self.config = config_json
         if len(sys.argv) <= 1:
             print self.__header_message(self.config["description"])
             print "Commands:\n"+self.__print_arguments()
@@ -15,9 +15,9 @@ class Cli:
                 if i+1 == len(sys.argv):
                     if "description" in self.config:
                         print self.__header_message(self.config["description"])
-                if not self.config:
+                if not self.config or "args" not in self.config:
                     if len(sys.argv[1:]) > i:
-                        print "Too much arguments!!"
+                        print self.__fatal_message("Too much arguments!!")
                         sys.exit()
                     return
                 else:
@@ -28,21 +28,49 @@ class Cli:
 
     def bcolor(self, color):
         colors = {
-        "HEADER": '\033[95m',
-        "OKBLUE": '\033[94m',
-        "OKGREEN": '\033[92m',
-        "WARNING": '\033[93m',
-        "FAIL": '\033[91m',
-        "ENDC": '\033[0m',
-        "BOLD": '\033[1m',
-        "UNDERLINE": '\033[4m'
+            "HEADER": '\033[95m',
+            "OKBLUE": '\033[94m',
+            "OKGREEN": '\033[92m',
+            "WARNING": '\033[93m',
+            "FAIL": '\033[91m',
+            "ENDC": '\033[0m',
+            "BOLD": '\033[1m',
+            "UNDERLINE": '\033[4m'
         }
         return colors[color]
 
+    """def bgcolor(self, color):
+        colors = {
+            "HEADER": '\033[95m',
+            "OKBLUE": '\033[94m',
+            "OKGREEN": '\033[92m',
+            "WARNING": '\033[93m',
+            "FAIL": '\033[91m',
+            "ENDC": '\033[0m',
+            "BOLD": '\033[1m',
+            "UNDERLINE": '\033[4m'
+        }
+        return colors[color]"""
+
+    def __get_redundance(self, arg):
+        redundance = ["\t","\t","\t","\t"]
+        for i in range(0, int(len(arg)/8)):
+            redundance.pop()
+        return "".join(redundance)
+
     def __print_arguments(self):
         msg = ""
+        i = 0
         for arg in self.config["args"]:
-            msg += "\t"+self.__warning_message(arg)+"\n"
+            redundance = self.__get_redundance(arg)
+            description = ""
+            if "description" in self.config["args"][arg]:
+                description = self.config["args"][arg]["description"]
+            if i % 2 == 0:
+                msg += "   "+self.__ok_message(arg + redundance + description + "\n")
+            else:
+                msg += "   "+self.__green_message(arg+redundance+ description + "\n")
+            i += 1
         return msg
 
     def __header_message(self, message):
@@ -54,6 +82,9 @@ class Cli:
     def __warning_message(self, message):
         return self.bcolor("WARNING") + message + self.bcolor("ENDC")
 
+    def __green_message(self, message):
+        return self.bcolor("OKGREEN") + message + self.bcolor("ENDC")
+
     def __ok_message(self, message):
         return self.bcolor("OKBLUE") + message + self.bcolor("ENDC")
 
@@ -63,8 +94,7 @@ class Cli:
         sys.exit()
 
     def __insufficent_argument(self):
-        print self.__fatal_message("Insufficent Arguments:")
+        print self.__fatal_message("Insufficent Arguments!!")
+        print self.__warning_message("Permitted Arguments:")
         print self.__print_arguments()
         sys.exit()
-
-Cli("cli.json")
